044f653609367b068dd8f61fe21d8df4787d0bf7
Francois Deppierraz <francois@ctrlaltdel.ch>
2020-07-10 11:15:10 +0200

    Move to protobuf

--- exposed.py
+++ exposed.py
@@ -4,12 +4,13 @@ import sys
 import time
 import requests
 import arrow
+import exposed_pb2
 from simplejson.errors import JSONDecodeError
 
-#URL = 'https://www.pt-d.bfs.admin.ch/v1/exposedjson/{}'
-#URL = 'https://www.pt-t.bfs.admin.ch/v1/exposedjson/{}'
-#URL = 'https://www.pt-a.bfs.admin.ch/v1/exposedjson/{}'
-URL = 'https://www.pt.bfs.admin.ch/v1/exposedjson/{}'
+#URL = 'https://www.pt-d.bfs.admin.ch/v1/exposed/{}'
+#URL = 'https://www.pt-t.bfs.admin.ch/v1/exposed/{}'
+#URL = 'https://www.pt-a.bfs.admin.ch/v1/exposed/{}'
+URL = 'https://www.pt.bfs.admin.ch/v1/exposed/{}'
 
 BATCH_LENGTH = 2 * 60 * 60 * 1000
 
@@ -21,16 +22,15 @@ while True:
 	batch_release_time = timestamp - (timestamp % BATCH_LENGTH)
 	r = requests.get(URL.format(batch_release_time))
 
-	try:
-		print(r.json())
-		exposed = exposed + r.json()['exposed']
-	except JSONDecodeError:
-		if r.content == b'':
-			break
-		else:
-			print('Error: ', timestamp, r.content)
+	exposed_list = exposed_pb2.ProtoExposedList.FromString(r.content)
 
-	sys.stdout.flush()
+
+	if exposed_list.batchReleaseTime:
+		exposed = exposed + list(exposed_list.exposed)
+		print(exposed_list)
+		sys.stdout.flush()
+	else:
+		break
 
 	timestamp -= BATCH_LENGTH
 
